931e1fa7bfa28e19aaed617714a9de4b808d9a3b
luan <luan_naul90@hotmail.com>
2022-07-27 11:32:16 -0300

    fix: Corrigido o direcionamento das rotas

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,7 @@ import { Router } from "./router.js";
 
 const router = new Router();
 
-router.add("/", "/assets/pages/home.html");
+router.add("/home", "/assets/pages/home.html");
 router.add("/universe", "/assets/pages/universe.html");
 router.add("/exploration", "/assets/pages/exploration.html");
 router.add(404, "/assets/pages/404.html");
@@ -13,34 +13,38 @@ window.onpopstate = () => router.handle();
 window.router = () => router.router();
 
 const body = document.querySelector("body");
-const home = document.querySelector("header a:nth-child(1)");
-const universe = document.querySelector("header a:nth-child(2)");
-const exploration = document.querySelector("header a:nth-child(3)");
+const home = document.querySelector(".bg__home");
+const universe = document.querySelector(".bg__universe");
+const exploration = document.querySelector(".bg__exploration");
 
+// console.log(body);
 // console.log(home);
 // console.log(universe);
 // console.log(exploration);
 
-home.addEventListener("click", function () {
-  body.classList.remove("universe");
-  body.classList.remove("exploration");
-  home.classList.add("active");
-  universe.classList.remove("active");
-  exploration.classList.remove("active");
+home.addEventListener("click", () => {
+  window.location = "home";
+  body.classList.remove("bg__universe");
+  body.classList.remove("bg__exploration");
+  // home.classList.add("active");
+  // universe.classList.remove("active");
+  // exploration.classList.remove("active");
 });
 
-universe.addEventListener("click", function () {
-  body.classList.remove("exploration");
-  body.classList.add("universe");
-  home.classList.remove("active");
-  universe.classList.add("active");
-  exploration.classList.remove("active");
+universe.addEventListener("click", () => {
+  window.location = "universe";
+  body.classList.remove("bg__exploration");
+  body.classList.add("bg__universe");
+  // home.classList.remove("active");
+  // exploration.classList.remove("active");
+  // universe.classList.add("active");
 });
 
-exploration.addEventListener("click", function () {
-  body.classList.remove("universe");
-  body.classList.add("exploration");
-  home.classList.remove("active");
-  universe.classList.remove("active");
-  exploration.classList.add("active");
+exploration.addEventListener("click", () => {
+  window.location = "exploration";
+  body.classList.remove("bg__universe");
+  body.classList.add("bg__exploration");
+  // home.classList.remove("active");
+  // universe.classList.remove("active");
+  // exploration.classList.add("active");
 });
